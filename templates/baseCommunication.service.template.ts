198f169a9ac40a21833ad6a1b0523268af522bbf
Kevin Krone <krone@genodata.de>
2020-11-24 17:54:26 +0100

    Template fix

--- templates/baseCommunication.service.template.ts
+++ templates/baseCommunication.service.template.ts
@@ -5,10 +5,10 @@ declare var command: any;
 export class BaseCommunicationService 
 {
   //Wrapper um Function mit Callbacks als Promise verwenden zu können
-  private async apiCommand<Response> (verb: "POST" | "GET", command: string, payload: object): Promise<Response>
+  private async apiCommand<Response> (verb: "POST" | "GET", commandStr: string, payload: object): Promise<Response>
   {
     return new Promise(function(resolve, reject) {
-      command('open_api', JSON.stringify({verb, command, "json": JSON.stringify(payload)}), resolve, reject);
+      command('open_api', JSON.stringify({verb, command: commandStr, "json": JSON.stringify(payload)}), resolve, reject);
     });
   }
   [[ENDPOINTS]]
